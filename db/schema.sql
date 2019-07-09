9d9475eb9c461b48d6509f9b2b08e06bd0874a0f
Bryton Smith <bryton.jay.smith@gmail.com>
2019-07-09 03:17:22 -0700

    initial

---
+++ db/schema.sql
@@ -0,0 +1,15 @@
+	
+DROP DATABASE [IF EXISTS] burgers_db;
+
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers pet (
+    id INT NOT NULL AUTO_INCREMENT, 
+    burger_name VARCHAR(20), 
+    devoured BOOLEAN, 
+    PRIMARY KEY (id)
+)
+       
+
